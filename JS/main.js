7f05538696ffb7fec3fd8cdf2e979677514384cd
Kate Hrynchak <katinagg@gmail.com>
2020-11-12 19:19:53 +0200

    JS - lessons-2

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,11 @@
-alert('Привет');
+"use strict"
+
+let user = "John"
+let age = 25;
+let bigNumber = 1000200030004000500060007000n;
+let trueAge = true;
+let ageTrue = null;
+let place;
+
+
+console.log(`string - ${user}, boolean - ${trueAge}, number - ${age}` + ", bigint - " + bigNumber + ", \"underfined\" - " + place + ", \'null\' - " + ageTrue);
