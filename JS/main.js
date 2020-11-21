2d716390e7162354f9a4f59303d4ce43e5f9e69d
katinahh <katinagg@gmail.com>
2020-11-21 17:38:24 +0200

    JS Sat 2020-11-21

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,57 @@
-"use strict"
+// "use strict"
 
-let user = "John"
-let age = 25;
-let bigNumber = 1000200030004000500060007000n;
-let trueAge = true;
-let ageTrue = null;
-let place;
+// let user = "John"
+// let age = 25;
+// let bigNumber = 1000200030004000500060007000n;
+// let trueAge = true;
+// let ageTrue = null;
+// let place;
 
 
-console.log(`string - ${user}, boolean - ${trueAge}, number - ${age}` + ", bigint - " + bigNumber + ", \"underfined\" - " + place + ", \'null\' - " + ageTrue);
+// console.log(`string - ${user}, boolean - ${trueAge}, number - ${age}` + ", bigint - " + bigNumber + ", \"underfined\" - " + place + ", \'null\' - " + ageTrue);
+
+// alert("Hello World");
+///////////////////////////////////////////////////
+// let massage;
+// massage = 'Hi';
+// massage = 'Hi again!';
+// massage = 'Doh';
+
+// console.log(massage);
+// //////////////////////////////////////////////////
+// let one = 'Hou';
+// let two;
+// two = one;
+
+// console.log(one);
+// console.log(two);
+// //////////////////////////////////////////////////
+// let admin;
+// let name = 'John';
+
+// admin = name;
+// console.log(admin);
+
+// String ////////////////////////////////////////////////////
+// let name = "Kate";
+// console.log(`hi, ${name}`);
+
+// // Boolean ///////////////////////////////////////////////
+// let age = 3>4;
+// console.log(age);
+
+// let age = prompt('Сколько тебе лет?', 100);
+// alert(`Тебе ${age} лет!`); // Тебе 100 лет!
+
+//prompt("How old are you?");
+
+// let age1 = prompt('How old are you', 18);
+// alert(`WOW, you are ${age1} age`);
+
+// let isBoss = confirm("Ты здесь главный?");
+// alert( isBoss );
+
+// let value = true;
+// console.log(typeof value);
+
+console.log(4%3);
